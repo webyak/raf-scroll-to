83a3b0054ce8e6a858adac61cdc6da4f1a4726a3
webyak <webyak@outlook.com>
2015-08-26 13:27:37 +0200

    Fix code formatting.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var raf = require('raf');
 
-Math.easeInOutQuad = function (t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t + b;
+var easeInOutQuad = function (t, b, c, d) {
+	t /= d / 2;
+	if (t < 1) {
+		return c / 2 * t * t + b;
+	}
 	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
+	return -c / 2 * (t * (t - 2) - 1) + b;
 };
 
 /**
 * Scroll to a DOM element
-*
 * @param {Element} element  - The element to scroll to.
 * @param {number}  to       - The position to scroll to, relative to the top
 *                           	of the element.
 * @param {number}  duration - How long the scrolling should take.
 */
-function scrollTo(element, to, duration) {
+var scrollTo = function (element, to, duration) {
     var start = element.scrollTop,
         change = to - start,
         currentTime = 0,
@@ -23,13 +24,13 @@ function scrollTo(element, to, duration) {
 
     var animateScroll = function(){
         currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        var val = easeInOutQuad(currentTime, start, change, duration);
         element.scrollTop = val;
         if(currentTime < duration) {
             raf(animateScroll);
         }
     };
     animateScroll();
-}
+};
 
 module.exports = scrollTo;
